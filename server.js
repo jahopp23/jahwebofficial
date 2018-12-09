1037ab1068ee00241fcded0f1855eebe94b33175
John A. Hopper <jahopp@gmail.com>
2018-12-08 22:42:21 -0600

    images bootstrap cards

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var app = express();
 
 // Use Express's built-in static file server
 app.use(express.static('public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+// app.use('/js', express.static(__dirname + '/node_modules/popper/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 // Also respond to `GET` requests at the path '/hi'
 app.get('/hi', function(req, res){
